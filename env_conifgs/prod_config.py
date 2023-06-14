cdcf59f3442a6f45cf6ae8331c4ae42882ca44f5
alex olivares <alexander.b.olivares@gmail.com>
2023-06-13 20:40:52 -0500

    test with chrome options

--- env_conifgs/prod_config.py
+++ env_conifgs/prod_config.py
@@ -3,25 +3,25 @@ from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 import psycopg2
 
-# chrome_options = Options()
+chrome_options = Options()
 # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-# chrome_options.add_argument("--no-sandbox")
-# chrome_options.add_argument("--window-size=1920,1080")
-# chrome_options.add_argument('--headless')
-# chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--window-size=1920,1080")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
 # driver = webdriver.Chrome(executable_path=os.environ.get(
 #     "CHROMEDRIVER_PATH"), options=chrome_options)
 
 
-driver = webdriver.Chrome()
-sentry_dsn = os.environ.get('SENTRY_DSN')
-db_url = os.environ.get('DATABASE_URL')
+driver = webdriver.Chrome(options=chrome_options)
+sentry_dsn = os.environ.get("SENTRY_DSN")
+db_url = os.environ.get("DATABASE_URL")
 conn = psycopg2.connect(db_url)
 cursor = conn.cursor()
 
 config = {
-    'connection': conn,
-    'cursor': cursor,
-    'driver': driver,
-    'sentry_dsn': sentry_dsn
+    "connection": conn,
+    "cursor": cursor,
+    "driver": driver,
+    "sentry_dsn": sentry_dsn,
 }
