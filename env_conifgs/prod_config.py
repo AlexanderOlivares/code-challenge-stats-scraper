42be3dffbbdaaffd6bb0fd927fc2ed5e5e01bd1e
alex olivares <alexander.b.olivares@gmail.com>
2023-06-13 20:50:02 -0500

    Add cron schedule

--- env_conifgs/prod_config.py
+++ env_conifgs/prod_config.py
@@ -4,13 +4,10 @@ from selenium import webdriver
 import psycopg2
 
 chrome_options = Options()
-# chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--window-size=1920,1080")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
-# driver = webdriver.Chrome(executable_path=os.environ.get(
-#     "CHROMEDRIVER_PATH"), options=chrome_options)
 
 
 driver = webdriver.Chrome(options=chrome_options)
